6c3f3cf1a6a9be661397e248922b0bead95e313e
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2020-02-14 01:04:54 -0600

    Updates

---
+++ precache-manifest.a5a8328d0dde2c3115a5d619f52fb8fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e28eddb8e85c213886e88a0669ae871",
+    "url": "/bodymass-frontend/index.html"
+  },
+  {
+    "revision": "2adb32701fec6eb948bd",
+    "url": "/bodymass-frontend/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "da62dae86e011e7cdfef",
+    "url": "/bodymass-frontend/static/css/main.25a1e9b4.chunk.css"
+  },
+  {
+    "revision": "2adb32701fec6eb948bd",
+    "url": "/bodymass-frontend/static/js/2.9a412b1a.chunk.js"
+  },
+  {
+    "revision": "004c8ddb448ab3a8a4574e6f18483e3a",
+    "url": "/bodymass-frontend/static/js/2.9a412b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da62dae86e011e7cdfef",
+    "url": "/bodymass-frontend/static/js/main.68662141.chunk.js"
+  },
+  {
+    "revision": "8d48f688bce6572a5550",
+    "url": "/bodymass-frontend/static/js/runtime-main.140a4db3.js"
+  }
+]);
